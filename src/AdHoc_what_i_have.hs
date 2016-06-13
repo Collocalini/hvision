f9eddfc939519c5eb774d09ac12128105d0b41e5
hokum <hokum@localhost>
2016-06-13 22:38:15 +0300

    thinking out aloud in adhoc what i have

--- src/AdHoc_what_i_have.hs
+++ src/AdHoc_what_i_have.hs
@@ -10,18 +10,40 @@ Finally implementing all those drawings i made
 
 import qualified Data.Array.Repa as R
 --import qualified Data.Graph as G
+import Data.Conduit
+import Control.Monad.State
+
+
+
+type Graph = Maybe (R.Array R.U (R.Z :. Integer) GraphCell)
+type Memory = R.Array R.D (R.Z :. Integer) Cell
+
+
+
+
+data Cell = Cell {
+    value :: Maybe Rational
+ --  ,
+   }
+
+
+
+data GraphCell = Cell {
+    value :: Maybe Rational
+   ,neighbors :: Maybe (R.Array R.D (R.Z :. Integer) Integer)
+   }
+
 
 
 
 data MainContext = MainContext {
-   --memory :: Array D (Z :. Integer) Rational
-  --,memory_cache :: Array U (Z :. Integer) Rational
-  --,index_cache  :: Array U (Z :. Integer) Integer
-   memory :: [R.Array R.U (R.Z :. Integer) Rational]
+
+    memory :: Memory
       {-size of each new repa array should be increased exponentially (each 2 times
         bigger then the previous)-}
 
-
+   --,input  :: Graph
+   ---,output :: Graph
 
 
 
@@ -29,8 +51,55 @@ data MainContext = MainContext {
 
 
 
+--Sink Graph (State MainContext IO) ()
 
-mainLoop :: Integer -> MainContext Rational
+--getInput :: StateT MainContext IO
+
+
+{--From various formats into Graph--}
+sense :: Source (StateT MainContext IO) Graph
+
+
+{--Manage memory issues.
+   Sorting and caching for fast access is happening here
+   --}
+remember :: Conduit (StateT MainContext IO) Memory
+
+{--Extrapolate memories using predefined heuristics.
+   This is the step where only exact resolutions(конкретные воспоминания) of memories are formed.
+   --}
+dream :: Conduit (StateT MainContext IO) Memory
+
+{--Form abstractions(обобщения) over memories.
+   Different convolutions(свёртки) are happening here.
+   --}
+think :: Conduit (StateT MainContext IO) Memory
+
+{--Use memories to form an answer.
+   An answer is a piece of memory consisting of a number of cells activated by sences.
+   --}
+say :: Sink Memory (StateT MainContext IO) Graph
+
+
+
+
+theAIExperiment :: MainContext -> IO
+theAIExperiment mc = do
+   runStateT mainLoop mc
+
+
+
+{-mainLoopIO :: StateT MainContext IO
+mainLoopIO = do
+   getInput
+   mainLoop
+-}
+
+mainLoop :: StateT MainContext IO Graph
+mainLoop mc = sense $= remember =$= dream =$= think =$ say
+
+
+
 
 
 
